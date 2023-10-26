513d17815e7ad2e0dfc702522a5ff91a4ceb3ebf
Sudharsan Selvaraj <sselvarj@amazon.com>
2023-10-26 11:17:15 +0530

    Fix store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers";
 import saga from "./sagas";
 import { UIState } from "./reducers/ui";
@@ -10,7 +10,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
   reducer: rootReducer,
-  enhancers: [composeWithDevTools(applyMiddleware(sagaMiddleware))],
+  enhancers: [applyMiddleware(sagaMiddleware)],
 });
 
 sagaMiddleware.run(saga);
